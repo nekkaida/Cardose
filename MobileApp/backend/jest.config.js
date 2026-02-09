758d5d0c8770cb9a5105eb92bd7899735bca0528
nekkaida <kennethriadinugroho@gmail.com>
2026-02-09 16:05:37 +0700

    chore(backend): add Jest configuration

---
+++ MobileApp/backend/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/database/init.js',
+    '!src/database/seed.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  verbose: true,
+  testTimeout: 30000,
+  setupFilesAfterEnv: ['./tests/setup.js'],
+  moduleFileExtensions: ['js', 'json'],
+  clearMocks: true,
+  restoreMocks: true
+};
