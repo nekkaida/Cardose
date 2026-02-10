d3b6fb691687595ad049f2220b57820a49fcd378
nekkaida <kennethriadinugroho@gmail.com>
2026-02-10 22:22:42 +0700

    fix(web): suppress useEffect exhaustive-deps warning in AnalyticsPage

--- MobileApp/web-dashboard/src/pages/AnalyticsPage.tsx
+++ MobileApp/web-dashboard/src/pages/AnalyticsPage.tsx
@@ -12,6 +12,7 @@ const AnalyticsPage: React.FC = () => {
 
   useEffect(() => {
     loadAnalytics();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const loadAnalytics = async () => {
