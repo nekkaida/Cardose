4a934a77dd668044a8dbb4f9b6a8fd7a909f0d1b
nekkaida <kennethriadinugroho@gmail.com>
2026-02-21 18:02:50 +0700

    feat(dashboard): add barrel export for dashboard components

---
+++ MobileApp/web-dashboard/src/components/dashboard/index.ts
@@ -0,0 +1,8 @@
+export { default as KpiCards, KpiSkeleton } from './KpiCards';
+export { default as RevenueChart } from './RevenueChart';
+export { default as OrderStatusChart } from './OrderStatusChart';
+export { default as ProductionPipeline } from './ProductionPipeline';
+export { default as RecentActivity } from './RecentActivity';
+export { default as InventoryOverview } from './InventoryOverview';
+export { default as RevenueSummary } from './RevenueSummary';
+export { default as SectionError } from './SectionError';
